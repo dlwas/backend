43a3a8a61863320d2747c7dfe725406f942b9225
dlwas <57624006+dlwas@users.noreply.github.com>
2021-10-05 15:59:16 +0100

    Update subregion.js

--- modes/subregion.js
+++ modes/subregion.js
@@ -4,7 +4,6 @@ export default function ({ db, getRandomIds, rounds, checkLang }) {
   const results = []
   for (let i = 0; i < rounds; i++) {
     const good = [getRandomIds[i][0]].map((itemId) => db[itemId])[0]
-    console.log([getRandomIds[i][0]].map((itemId) => db[itemId])[0])
 
     const randoms = getRandomIds[i][1]
       .map((itemId) => [db[itemId].subregion[checkLang]])
